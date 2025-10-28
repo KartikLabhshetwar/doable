41a4cef759cfd7c324e6638f9ae6a6441876b7c8
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2025-10-28 14:24:29 +0530

    refactor: moved PrismaClient creation into function, added conditional logging,

--- lib/db.ts
+++ lib/db.ts
@@ -4,10 +4,15 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
+function createPrismaClient() {
+  return new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query'] : [],
   })
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+export const db = globalForPrisma.prisma ?? createPrismaClient()
+
+// In development, save Prisma client to global to avoid multiple instances
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = db
+}
